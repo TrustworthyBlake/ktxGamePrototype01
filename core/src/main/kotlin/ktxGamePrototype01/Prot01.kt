763780aeb11f1d40314898ebf228ed88f1478010
andrbl <andreasblakli@gmail.com>
2021-01-29 14:24:56 +0100

    Refactored main source files from java to kotlin

---
+++ core/src/main/kotlin/ktxGamePrototype01/Prot01.kt
@@ -0,0 +1,10 @@
+package com.github.TrustworthyBlake.ktxGamePrototype01
+
+import com.badlogic.gdx.Game
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
+class Prot01 : Game() {
+    override fun create() {
+        setScreen(FirstScreen())
+    }
+}
