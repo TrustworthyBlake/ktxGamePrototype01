78550353a72aaa6a7c4cea7670146cccdc8b26b9
andrbl <andreasblakli@gmail.com>
2021-02-01 14:35:39 +0100

    Added rendering and assests for the game. The game now renders a heart sprite on the first screen, switch between the views with the keys J and K

--- core/src/main/kotlin/ktxGamePrototype01/screen/FirstScreen.kt
+++ core/src/main/kotlin/ktxGamePrototype01/screen/FirstScreen.kt
@@ -3,7 +3,12 @@ package ktxGamePrototype01.screen
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ktx.app.KtxScreen
+import ktx.graphics.use
 import ktx.log.debug
 import ktx.log.logger
 import ktxGamePrototype01.Prot01
@@ -13,13 +18,26 @@ import ktxGamePrototype01.Prot01
 private val LOG = logger<FirstScreen>()
 
 class FirstScreen(game:Prot01) : AbstractScreen(game) {
+    val batch : Batch by lazy { SpriteBatch() }
+    private val texture = Texture(Gdx.files.internal("graphics/skill_icons16.png"))
+    private val sprite = Sprite(texture)
+
     override fun show() {
         LOG.debug { "First screen is displayed" }
+        sprite.setPosition(1f, 1f)
     }
 
     override fun render(delta: Float) {
         if(Gdx.input.isKeyJustPressed(Input.Keys.K)){
            game.setScreen<SecondScreen>()
         }
+       batch.use{
+            sprite.draw(it)
+       }
+    }
+
+    override fun dispose() {
+        texture.dispose()
+        batch.dispose()
     }
 }
