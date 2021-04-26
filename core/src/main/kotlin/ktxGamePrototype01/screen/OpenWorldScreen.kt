3bae80773f8d5dd9102956c6b5bcc169585c6caf
andrbl <andreasblakli@gmail.com>
2021-04-26 14:15:04 +0200

    added readTeachersFromFile() and createUserEntity()

--- core/src/main/kotlin/ktxGamePrototype01/screen/OpenWorldScreen.kt
+++ core/src/main/kotlin/ktxGamePrototype01/screen/OpenWorldScreen.kt
@@ -1,6 +1,9 @@
 package ktxGamePrototype01.screen
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Preferences
 import com.badlogic.gdx.utils.viewport.FitViewport
+import ktx.ashley.entity
 import ktx.log.logger
 import ktxGamePrototype01.Prot01
 
@@ -17,5 +20,26 @@ class OpenWorldScreen(game : Prot01) : AbstractScreen(game) {
     }
     override fun dispose() {}
 
+    // Get player data of sprite
+    // Get all teacher player data sprite
+    // Dynamically add the teachers to the map
+    // Make teachers intractable
+    // Quest marker if teacher has added new quiz user haven't completed yet
+    // Open up window which lets the user decide which quiz to do or place the different quizes as entities on the map
+    // When quiz is chosen, close the open world and open up the quiz screen
+    private fun createUserEntityFromFile(){
+        //val playerData =
+        val prefs: Preferences = Gdx.app.getPreferences("playerData") // + playerName string from app
+        val playerHead = prefs.getString("avatarHead")
+        val playerBody = prefs.getString("avatarBody")
+        if(playerBody != null || playerHead != null) {
+            val playerEntitiy = engine.entity {
 
+            }
+        }
+}
+
+    private fun readTeachersFromFile(){
+
+    }
 }
