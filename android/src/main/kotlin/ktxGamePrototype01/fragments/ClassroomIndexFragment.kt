06de84341578dd99dc5fd82a4ea20870d4c2c1ab
Vegardoa <voaarnes@gmail.com>
2021-05-02 17:50:20 +0200

    Users can create classrooms that are connected to database

--- android/src/main/kotlin/ktxGamePrototype01/fragments/ClassroomIndexFragment.kt
+++ android/src/main/kotlin/ktxGamePrototype01/fragments/ClassroomIndexFragment.kt
@@ -3,6 +3,7 @@ package ktxGamePrototype01.fragments
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,8 +19,10 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.github.trustworthyblake.ktxGamePrototype01.R
 import com.github.trustworthyblake.ktxGamePrototype01.databinding.FragmentClassroomIndexBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import ktxGamePrototype01.AppActivity
+import ktxGamePrototype01.DBObject
 import ktxGamePrototype01.Prot01
 import ktxGamePrototype01.User
 import ktxGamePrototype01.adapters.Chat
@@ -27,6 +30,8 @@ import ktxGamePrototype01.adapters.ClassroomIndexRecyclerAdapter
 import java.util.*
 import kotlin.collections.ArrayList
 
+private val failTAG = "DATABASE ENTRY FAILED"
+private val successTAG = "DATABASE ENTRY SUCCESS"
 
 class ClassroomIndexFragment : Fragment() {
     private lateinit var binding: FragmentClassroomIndexBinding
@@ -51,6 +56,9 @@ class ClassroomIndexFragment : Fragment() {
             val editText  = dialogLayout.findViewById<EditText>(R.id.editText)
             builder.setView(dialogLayout)
             builder.setPositiveButton("OK") { dialogInterface, i ->
+
+                addClassroom(DBObject.capitalize(editText.text.toString()), "3rd", "Teacher test", "2021".toInt(), userID)
+
                 classList.add(editText.text.toString())
                 adapter.notifyItemInserted(classList.size - 1)
             }
@@ -69,6 +77,42 @@ class ClassroomIndexFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun addClassroom(courseName: String, grade: String, teacher: String, year: Int, uid: String) {
+        val db = FirebaseFirestore.getInstance()
+        // create data entry for the course
+        val studentList: List<String> = emptyList()
+        val announcementList: List<String> = emptyList()
+        val modules: List<String> = emptyList()
+        //val userID = findUserByName(capitalize(teacher))
+
+        val course = hashMapOf(
+            "course name" to courseName,
+            "grade" to grade,
+            "modules" to modules,
+            "teacher name" to DBObject.capitalize(teacher),
+            "year" to year,
+            "students" to studentList,
+            "announcements" to announcementList
+        )
+        // add the data into the database
+        db.collection("classrooms").document("$grade grade $courseName $year")
+            .set(course)
+            .addOnSuccessListener { Log.d(successTAG, "Successfully added classroom to DB")
+                Toast.makeText(activity, "Classroom successfully created", Toast.LENGTH_LONG).show()
+                findNavController().navigate(R.id.dest_user)}
+            .addOnFailureListener { e -> Log.w(failTAG, "Error adding classroom to DB", e)
+                Toast.makeText(activity, "Classroom creation error", Toast.LENGTH_LONG).show()}
+
+        db.collection("classrooms")
+            .document("$grade grade $courseName $year")
+            .update("students", FieldValue.arrayUnion(uid))
+
+        db.collection("users")
+            .document(uid)
+            .update("courses", FieldValue.arrayUnion("$grade grade $courseName $year"))
+
     }
 
     private fun fetchClassrooms(id: String ){
